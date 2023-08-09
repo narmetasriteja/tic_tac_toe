9cc80a3e7574d95dd319fd707543e08ebe168de5
narmetasriteja <137908784+narmetasriteja@users.noreply.github.com>
2023-08-09 21:37:48 +0530

    Add files via upload

---
+++ #Mini project on a random puzzle of.py
@@ -0,0 +1,49 @@
+#Mini project on a random puzzle of a Suduko
+M = 9
+def puzzle(a):
+    for i in range(M):
+        for j in range(M):
+            print(a[i][j],end = " ")
+        print()
+def solve(grid, row, col, num):
+    for x in range(9):
+        if grid[row][x] == num:
+            return False
+    for x in range(9):
+        if grid[x][col] == num:
+            return False
+    startRow = row - row % 3
+    startCol = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if grid[i + startRow][j + startCol] == num:
+                return False
+    return True
+def Suduko(grid, row, col):
+    if (row == M - 1 and col == M):
+        return True
+    if col == M:
+        row += 1  
+        col = 0
+    if grid[row][col] > 0:
+        return Suduko(grid, row, col + 1)
+    for num in range (1, M + 1, 1):
+        if solve(grid, row, col, num):
+            grid[row][col] = num
+            if Suduko(grid, row, col + 1):
+                return True
+        grid[row][col] = 0
+    return False
+grid= [[1, 5, 0, 0, 4, 2, 0, 0, 6],
+       [2, 7, 4, 5, 6, 0, 0, 1, 0],
+       [0, 0, 6, 0, 0, 7, 4, 0, 2],
+       [0, 1, 0, 0, 0, 0, 0, 4, 0],
+       [0, 0, 0, 0, 5, 0, 0, 0, 0],
+       [0, 6, 0, 4, 0, 3, 1, 9, 0],
+       [0, 2, 0, 6, 0, 5, 9, 0, 0],
+       [9, 8, 5, 0, 3, 0, 0, 6, 0],
+       [0, 4, 0, 2, 1, 9, 8, 3, 0]]
+if (Suduko(grid,0,0)):
+    puzzle(grid)
+else:
+    print("no solution")
